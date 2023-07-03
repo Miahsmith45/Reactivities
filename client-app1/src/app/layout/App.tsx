658a3a6000a3934ce4526b78af58a9142101b4ba
Miahsmith45 <veryjeremiah@gmail.com>
2023-07-03 15:27:00 -0400

    End of section 6

--- client-app1/src/app/layout/App.tsx
+++ client-app1/src/app/layout/App.tsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from './models/activity';
 import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import agent from './api/agent';
+import LoadingComponent from './LoadingComponents';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedACtivity] = useState<Activity | undefined>(undefined)
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities')
-      .then(response => {
-        setActivities(response.data);
+  agent.Activities.list().then(response => {
+      let activities: Activity[] = [];
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity)
+      })
+        setActivities(activities);
+        setLoading(false);
       })
 
   }, [])
@@ -35,15 +43,34 @@ function App() {
   }
   
 function handleCreateOrEditActivity(activity:Activity){
-  activity.id
-   ? setActivities([...activities.filter(e=>e.id !== activity.id), activity])
-   : setActivities([...activities, {...activity, id: crypto.randomUUID()}]);
-   setEditMode(false);
-   setSelectedACtivity(activity);
+  setSubmitting(true);
+  if (activity.id) {
+    agent.Activities.update(activity).then(()=>{
+      setActivities([...activities.filter(e=>e.id !== activity.id), activity])
+      setSelectedACtivity(activity)
+      setEditMode(false);
+      setSubmitting(false);
+    })
+  }else {
+    activity.id= crypto.randomUUID();
+    agent.Activities.create(activity).then(()=>{
+      setActivities([...activities, activity])
+      setSelectedACtivity(activity)
+      setEditMode(false);
+      setSubmitting(false);
+    })  
+  }
 }
 function handleDeleteActivity(id: string) {
-  setActivities([...activities.filter(e=>e.id !== id)])
+  setSubmitting(true);
+  agent.Activities.delete(id).then(()=>{
+    setActivities([...activities.filter(e=>e.id !== id)])
+    setSubmitting(false);
+  })
+ 
 }
+
+if(loading) return <LoadingComponent content='Loading app'/>
   return (
     <>
       <NavBar openForm={handleFormOpen} />
@@ -58,6 +85,7 @@ function handleDeleteActivity(id: string) {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
 
       </Container>
